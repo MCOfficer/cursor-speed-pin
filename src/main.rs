9ce80adf036b19694e9d2597adb2d433d477341e
MCOfficer <mcofficer@gmx.de>
2021-01-03 20:16:45 +0100

    Implement desktop notifications, thank you @AmaranthineCodices

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ fn main() -> Result<()> {
         .unwrap();
 
     DESIRED_SPEED.store(winapi::get_mouse_speed()?, Ordering::SeqCst);
-    update_systray(&mut tray_icon, &green_icon, &red_icon);
+    update_status(&mut tray_icon, &green_icon, &red_icon);
 
     let checking_thread = std::thread::spawn(|| loop {
         std::thread::sleep_ms(50);
@@ -42,10 +42,10 @@ fn main() -> Result<()> {
             let current = winapi::get_mouse_speed().unwrap();
             let desired = DESIRED_SPEED.load(Ordering::Relaxed);
             if desired != current {
-                println!(
-                    "Got value of {}, resetting to desired speed {}",
+                winapi::notify(format!(
+                    "Speed was set to {}, resetting to desired speed {}",
                     current, desired
-                );
+                ));
                 winapi::set_mouse_speed(desired).unwrap();
             }
         }
@@ -65,7 +65,7 @@ fn main() -> Result<()> {
                         DESIRED_SPEED.store(winapi::get_mouse_speed().unwrap(), Ordering::SeqCst);
                         ENABLED.store(true, Ordering::SeqCst);
                     }
-                    update_systray(&mut tray_icon, &green_icon, &red_icon);
+                    update_status(&mut tray_icon, &green_icon, &red_icon);
                 }
             },
             _ => (),
@@ -73,17 +73,22 @@ fn main() -> Result<()> {
     });
 }
 
-fn update_systray<T>(tray_icon: &mut TrayIcon<T>, green: &Icon, red: &Icon)
+fn update_status<T>(tray_icon: &mut TrayIcon<T>, green: &Icon, red: &Icon)
 where
     T: PartialEq + Clone + 'static,
 {
     if ENABLED.load(Ordering::Relaxed) {
+        winapi::notify(format!(
+            "Pinning cursor speed of {}",
+            DESIRED_SPEED.load(Ordering::Relaxed)
+        ));
         tray_icon.set_tooltip(&format!(
             "Enabled, Speed: {}",
             DESIRED_SPEED.load(Ordering::Relaxed)
         ));
         tray_icon.set_icon(green);
     } else {
+        winapi::notify("Unpinned cursor speed".to_string());
         tray_icon.set_tooltip("Disabled");
         tray_icon.set_icon(red);
     }
@@ -92,8 +97,65 @@ where
 mod winapi {
     use anyhow::anyhow;
     use anyhow::Result;
+    use std::os::windows::ffi::OsStrExt;
+    use winapi::_core::borrow::BorrowMut;
+    use winapi::shared::guiddef::GUID;
+    use winapi::shared::winerror;
+    use winapi::um::combaseapi::CoCreateGuid;
+    use winapi::um::shellapi::{self, Shell_NotifyIconW, NOTIFYICONDATAW};
     use winapi::um::winnt::PVOID;
     use winapi::um::winuser::{SystemParametersInfoW, SPI_GETMOUSESPEED, SPI_SETMOUSESPEED};
+
+    macro_rules! encode_string_wide {
+        ($string:expr, $length:expr) => {{
+            let codepoints = std::ffi::OsString::from($string)
+                .as_os_str()
+                .encode_wide()
+                .take(64)
+                .collect::<Vec<u16>>();
+            let mut array = [0u16; $length];
+
+            unsafe {
+                std::ptr::copy_nonoverlapping(
+                    codepoints.as_ptr(),
+                    array.as_mut_ptr(),
+                    codepoints.len().min($length),
+                );
+            }
+            array
+        }};
+    }
+
+    fn generate_guid() -> Result<GUID, i32> {
+        unsafe {
+            let mut gen_guid: GUID = Default::default();
+            let result = CoCreateGuid(&mut gen_guid);
+            if result == winerror::S_OK {
+                Ok(gen_guid)
+            } else {
+                Err(result)
+            }
+        }
+    }
+
+    pub fn notify(message: String) {
+        std::thread::spawn(move || {
+            let mut notification_descriptor = NOTIFYICONDATAW {
+                cbSize: std::mem::size_of::<NOTIFYICONDATAW>() as u32,
+                uFlags: shellapi::NIF_INFO | shellapi::NIF_REALTIME | shellapi::NIF_GUID,
+                dwInfoFlags: shellapi::NIIF_NOSOUND,
+                guidItem: generate_guid().expect("Unable to generate a new GUID"),
+                szInfoTitle: encode_string_wide!("CursorSpeedPin", 64),
+                szInfo: encode_string_wide!(message, 256),
+                ..Default::default()
+            };
+            let desc = notification_descriptor.borrow_mut();
+            unsafe { Shell_NotifyIconW(shellapi::NIM_ADD, &mut *desc) };
+
+            std::thread::sleep(std::time::Duration::from_secs(3));
+            unsafe { Shell_NotifyIconW(shellapi::NIM_DELETE, &mut *desc) };
+        });
+    }
 
     // Retrieves the currently set mouse speed. Returns a `u8` ranging from 1 to 20.
     pub fn get_mouse_speed() -> Result<u8> {
